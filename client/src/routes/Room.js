8f38142af03b3e1729dc37d7f697adc97d8f7bcc
tanisha.uni <jaintanisha52@gmail.com>
2021-07-14 02:51:14 +0530

    resolved db errors

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,181 +1,218 @@
 import React, { useRef, useEffect, useState } from "react";
 import io from "socket.io-client";
-import { PartnerMessage, PartnerRow, MyMessage, MyRow, ButtonMessage, MessageBox, Messages, CallText, Image, SendMessage } from '../styles/MessagesStyles'
-import '../styles/RoomStyles.css'
-import message from '../assets/images/message.png'
-import Header from '../components/Header'
-
+import {
+  PartnerMessage,
+  PartnerRow,
+  MyMessage,
+  MyRow,
+  ButtonMessage,
+  MessageBox,
+  Messages,
+  CallText,
+  Image,
+  SendMessage,
+} from "../styles/MessagesStyles";
+import "../styles/RoomStyles.css";
+import message from "../assets/images/message.png";
+import Header from "../components/Header";
 
 const Room = (props) => {
-    const userVideo = useRef();
-    const partnerVideo = useRef();
-    const peerRef = useRef();
-    const socketRef = useRef();
-    const otherUser = useRef();
-    const userStream = useRef();
-    const senders = useRef([]);
-    const sendChannel = useRef();
-    const [text, setText] = useState("");
-    const [messages, setMessages] = useState([]);
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const peerRef = useRef();
+  const socketRef = useRef();
+  const otherUser = useRef();
+  const userStream = useRef();
+  const senders = useRef([]);
+  const sendChannel = useRef();
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-            
-            navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-            userVideo.current.srcObject = stream;
-            userStream.current = stream;
+  useEffect(() => {
+    const runFunction = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        userVideo.current.srcObject = stream;
+        userStream.current = stream;
+      } catch (e) {
+        console.log(e);
+      }
 
-            try { socketRef.current = io.connect("/");  console.log('connection done')}
-                catch (err) { console.log(err) }
-            
-            try
-                {
-                socketRef.current.emit("join room", props.match.params.roomID);
-                console.log('join room working')
-                } catch(err) { console.log('error is' , err) }
+      try {
+        socketRef.current = io.connect("http://localhost:7000");
+        console.log("connection done");
+      } catch (err) {
+        console.log(err, "error");
+      }
 
-            socketRef.current.on('other user', userID => {
-                callUser(userID);
-                otherUser.current = userID;
-            });
+      try {
+        socketRef.current.emit("join room", props.match.params.roomID);
+        console.log("join room working");
+      } catch (err) {
+        console.log("error is", err);
+      }
 
-            socketRef.current.on("user joined", userID => {
-                otherUser.current = userID;
-            });
+      socketRef.current.on("other user", (userID) => {
+        callUser(userID);
+        otherUser.current = userID;
+      });
 
-            socketRef.current.on("offer", handleRecieveCall);
+      socketRef.current.on("user joined", (userID) => {
+        otherUser.current = userID;
+      });
 
-            socketRef.current.on("answer", handleAnswer);
+      socketRef.current.on("offer", handleRecieveCall);
 
-            socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
-           })
-           
+      socketRef.current.on("answer", handleAnswer);
 
-    }, []);
+      socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+    };
 
-//   useEffect(() => {
-//     document.addEventListener("visibilitychange", event => {
-//       if (document.visibilityState === 'hidden') {
-//         if (document.pictureInPictureElement) {
-//           document.exitPictureInPicture();
-//           }
-//       }
-//       })
-//     })
-    
-  
-    function callUser(userID) {
-        peerRef.current = createPeer(userID);
-        userStream.current.getTracks().forEach(track =>
+    runFunction();
+  }, []);
+
+  //   useEffect(() => {
+  //     document.addEventListener("visibilitychange", event => {
+  //       if (document.visibilityState === 'hidden') {
+  //         if (document.pictureInPictureElement) {
+  //           document.exitPictureInPicture();
+  //           }
+  //       }
+  //       })
+  //     })
+
+  function callUser(userID) {
+    peerRef.current = createPeer(userID);
+    userStream.current
+      .getTracks()
+      .forEach((track) =>
         senders.current.push(
-          peerRef.current.addTrack(track, userStream.current
-          )
+          peerRef.current.addTrack(track, userStream.current)
         )
       );
-      //
-      sendChannel.current = peerRef.current.createDataChannel("sendChannel")
-      sendChannel.current.onmessage = handleRecieveMessage;
-      //
+    //
+    sendChannel.current = peerRef.current.createDataChannel("sendChannel");
+    sendChannel.current.onmessage = handleRecieveMessage;
+    //
   }
   //
   function handleRecieveMessage(e) {
-    setMessages(messages => [...messages, {yours: false, value: e.data}])
+    setMessages((messages) => [...messages, { yours: false, value: e.data }]);
   }
   //
 
-    function createPeer(userID) {
-        const peer = new RTCPeerConnection({
-            iceServers: [
-                {
-                    urls: "stun:stun.stunprotocol.org"
-                },
-                {
-                    urls: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-            ]
-        });
+  function createPeer(userID) {
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+      ],
+    });
 
-        peer.onicecandidate = handleICECandidateEvent;
-        peer.ontrack = handleTrackEvent;
-        peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+    peer.onicecandidate = handleICECandidateEvent;
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
 
-        return peer;
-    }
+    return peer;
+  }
 
-    function handleNegotiationNeededEvent(userID) {
-        peerRef.current.createOffer().then(offer => {
-            return peerRef.current.setLocalDescription(offer);
-        }).then(() => {
-            const payload = {
-                target: userID,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            };
-            socketRef.current.emit("offer", payload);
-        }).catch(e => console.log(e));
-    }
+  function handleNegotiationNeededEvent(userID) {
+    peerRef.current
+      .createOffer()
+      .then((offer) => {
+        return peerRef.current.setLocalDescription(offer);
+      })
+      .then(() => {
+        const payload = {
+          target: userID,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("offer", payload);
+      })
+      .catch((e) => console.log(e));
+  }
 
-    function handleRecieveCall(incoming) {
-      peerRef.current = createPeer();
-      //
-      peerRef.current.ondatachannel = (event) => {
-        sendChannel.current = event.channel;
-        sendChannel.current.onmessage = handleRecieveMessage;
-      }
-        const desc = new RTCSessionDescription(incoming.sdp);
-        peerRef.current.setRemoteDescription(desc).then(() => {
-            userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
-        }).then(() => {
-            return peerRef.current.createAnswer();
-        }).then(answer => {
-            return peerRef.current.setLocalDescription(answer);
-        }).then(() => {
-            const payload = {
-                target: incoming.caller,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            }
-            socketRef.current.emit("answer", payload);
-        })
-    }
-
-    function handleAnswer(message) {
-        const desc = new RTCSessionDescription(message.sdp);
-        peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
-    }
-
-    function handleICECandidateEvent(e) {
-        if (e.candidate) {
-            const payload = {
-                target: otherUser.current,
-                candidate: e.candidate,
-            }
-            socketRef.current.emit("ice-candidate", payload);
-        }
-    }
-
-    function handleNewICECandidateMsg(incoming) {
-        const candidate = new RTCIceCandidate(incoming);
-
-        peerRef.current.addIceCandidate(candidate)
-            .catch(e => console.log(e));
-    }
-
-    function handleTrackEvent(e) {
-        partnerVideo.current.srcObject = e.streams[0];
+  function handleRecieveCall(incoming) {
+    peerRef.current = createPeer();
+    //
+    peerRef.current.ondatachannel = (event) => {
+      sendChannel.current = event.channel;
+      sendChannel.current.onmessage = handleRecieveMessage;
     };
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current
+      .setRemoteDescription(desc)
+      .then(() => {
+        userStream.current
+          .getTracks()
+          .forEach((track) =>
+            peerRef.current.addTrack(track, userStream.current)
+          );
+      })
+      .then(() => {
+        return peerRef.current.createAnswer();
+      })
+      .then((answer) => {
+        return peerRef.current.setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("answer", payload);
+      });
+  }
 
-    function shareScreen() {
-        navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
-            const screenTrack = stream.getTracks()[0];
-            senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
-            screenTrack.onended = function() {
-                senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
-            }
-        })
+  function handleAnswer(message) {
+    const desc = new RTCSessionDescription(message.sdp);
+    peerRef.current.setRemoteDescription(desc).catch((e) => console.log(e));
+  }
+
+  function handleICECandidateEvent(e) {
+    if (e.candidate) {
+      const payload = {
+        target: otherUser.current,
+        candidate: e.candidate,
+      };
+      socketRef.current.emit("ice-candidate", payload);
     }
+  }
 
+  function handleNewICECandidateMsg(incoming) {
+    const candidate = new RTCIceCandidate(incoming);
+
+    peerRef.current.addIceCandidate(candidate).catch((e) => console.log(e));
+  }
+
+  function handleTrackEvent(e) {
+    partnerVideo.current.srcObject = e.streams[0];
+  }
+
+  function shareScreen() {
+    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then((stream) => {
+      const screenTrack = stream.getTracks()[0];
+      senders.current
+        .find((sender) => sender.track.kind === "video")
+        .replaceTrack(screenTrack);
+      screenTrack.onended = function () {
+        senders.current
+          .find((sender) => sender.track.kind === "video")
+          .replaceTrack(userStream.current.getTracks()[1]);
+      };
+    });
+  }
 
   function requestPictureInPicture() {
     partnerVideo.current.requestPictureInPicture();
@@ -183,65 +220,65 @@ const Room = (props) => {
 
   function handleChange(e) {
     setText(e.target.value);
-}
+  }
 
   function sendMessage() {
-    sendChannel.current.send(text)
-    setMessages(messages => [...messages, { yours: true, value: text }])
-    setText('');
+    sendChannel.current.send(text);
+    setMessages((messages) => [...messages, { yours: true, value: text }]);
+    setText("");
   }
 
   function renderMessage(message, index) {
     if (message.yours) {
-        return (
-            <MyRow key={index}>
-                <MyMessage>
-                    {message.value}
-                </MyMessage>
-            </MyRow>
-        )
+      return (
+        <MyRow key={index}>
+          <MyMessage>{message.value}</MyMessage>
+        </MyRow>
+      );
     }
 
     return (
-        <PartnerRow key={index}>
-            <PartnerMessage>
-                {message.value}
-            </PartnerMessage>
-        </PartnerRow>
-    )
-}
-    return (
-        <div className='container-room'>
-            <div className='body-room'>
-                <div className='videos-room'>
-                    <div style={{ display: 'flex' }}>
-                    <video className='video' muted autoPlay ref={userVideo} />
-                    <video className='video' autoPlay ref={partnerVideo} /> 
-                    </div>
+      <PartnerRow key={index}>
+        <PartnerMessage>{message.value}</PartnerMessage>
+      </PartnerRow>
+    );
+  }
+  return (
+    <div className="container-room">
+      <div className="body-room">
+        <div className="videos-room">
+          <div style={{ display: "flex" }}>
+            <video className="video" muted autoPlay ref={userVideo} />
+            <video className="video" autoPlay ref={partnerVideo} />
+          </div>
 
-                    <div className ='buttons'>
-                    <button className = 'share-button' onClick={shareScreen}>Share screen</button>
-                    <button className = 'picture-button' onClick={requestPictureInPicture}>Picture In Picture</button>
-                    </div>
-                </div>
-
-                <div className='chat-room'>
-                    <CallText>In-Call Messages</CallText>
-                 
-                <Messages className = "messages">
-                 {messages.map(renderMessage)}
-                </Messages>
-                <SendMessage >
-                    <MessageBox value={text} onChange={handleChange} placeholder="  " />
-                    < Image
-                            src={message}
-                            onClick={sendMessage}
-                    />
-                </SendMessage>
-                </div>
-            </div>
+          <div className="buttons">
+            <button className="share-button" onClick={shareScreen}>
+              Share screen
+            </button>
+            <button
+              className="picture-button"
+              onClick={requestPictureInPicture}
+            >
+              Picture In Picture
+            </button>
+          </div>
         </div>
-    )
+
+        <div className="chat-room">
+          <CallText>In-Call Messages</CallText>
+
+          <Messages className="messages">
+            {messages.map(renderMessage)}
+          </Messages>
+          <SendMessage>
+            <MessageBox value={text} onChange={handleChange} placeholder="  " />
+            <Image src={message} onClick={sendMessage} />
+          </SendMessage>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Room;
--- index.js
+++ index.js
@@ -1,66 +1,82 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://link:1234567890q@link.aty5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const connection = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }) .then(() => {
-  console.log('Mongoose connected to db');
-})
-.catch((err) => {
-  console.log(err.message);
+const mongoose = require("mongoose");
+const uri =
+  "mongodb+srv://link:1234567890q@link.aty5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connection = mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "LinkDB",
+    authSource: "admin",
+  })
+  .then(() => {
+    console.log("Mongoose connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongodb bridge connected");
 });
 
-mongoose.connection.on('connected', () => {
-console.log('Mongodb bridge connected');
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection ERROR: ${err}`);
 });
 
-mongoose.connection.on('error', (err) => {
-console.log(`Mongoose connection ERROR: ${err}`);
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection disconnected");
 });
-
-mongoose.connection.on('disconnected', () => {
-console.log('Mongoose connection disconnected');
-});
-
-
 
 // import { Schema, model } from 'mongoose';
 
-const roomsSchema = new mongoose.Schema({ roomId: { type: String, default: null }, users: { type: Array, default: null } })
-const roomsModel = mongoose.model('rooms', roomsSchema)
+const roomsSchema = new mongoose.Schema({
+  roomId: { type: String, default: null },
+  users: { type: Array, default: [] },
+});
+const roomsModel = mongoose.model("rooms", roomsSchema);
 
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 
 const io = socket(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
 
 const rooms = {};
 
-io.on("connection", socket => {
-  console.log('backend up', socket.id)
-  socket.on("join room", roomID => {
-    const room = roomsModel.rooms.findOne({roomId: roomID});
-    if (room) {
-      room.users.push(socket.id)
-      console.log('room already exists')
-    } else {
-      room.roomID = roomID
-      room.users.push(socket.id)
-    }
-    room.save()
+io.on("connection", (socket) => {
+  console.log("backend up", socket.id);
+  socket.on("join room", async (roomID) => {
+    const room = await roomsModel.findOne({ roomId: roomID });
+    console.log(room);
 
-    const otherUser = room.users.find(id => id !== socket.id);
+    if (room) {
+      room.users.push(socket.id);
+      console.log("room already exists");
+      room.save();
+    } else {
+      // room.roomID = roomID
+      // room.users.push(socket.id)
+      const newRoom = await new roomsModel({
+        roomId: roomID,
+        users: [socket.id],
+      });
+      newRoom.save();
+    }
+
+    const otherUser = room.users.find((id) => id !== socket.id);
     if (otherUser) {
       socket.emit("other user", otherUser);
       socket.to(otherUser).emit("user joined", socket.id);
@@ -68,29 +84,27 @@ io.on("connection", socket => {
     }
   });
 
-  socket.on("offer", payload => {
+  socket.on("offer", (payload) => {
     io.to(payload.target).emit("offer", payload);
   });
 
-  socket.on("answer", payload => {
+  socket.on("answer", (payload) => {
     io.to(payload.target).emit("answer", payload);
   });
 
-  socket.on("ice-candidate", incoming => {
+  socket.on("ice-candidate", (incoming) => {
     io.to(incoming.target).emit("ice-candidate", incoming.candidate);
   });
-
 });
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(express.static(path.join(__dirname, './client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 
-
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 7000;
 
 server.listen(port, () => console.log(`server is running on port ${port}`));
 
