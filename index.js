dd35f7c0ac199a9fd2a5c9d26742ac5fb943d8a9
tanisha.uni <jaintanisha52@gmail.com>
2021-07-14 03:13:45 +0530

    build files

--- index.js
+++ index.js
@@ -97,10 +97,12 @@ io.on("connection", (socket) => {
   });
 });
 
+console.log(path.join(__dirname, "/client/build/index.html"));
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }
 
